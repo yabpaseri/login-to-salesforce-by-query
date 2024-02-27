97c59394f80ad8fbd524982d46b0da4689b44f37
yabpaseri <yab.82.hiro@gmail.com>
2024-02-27 14:36:57 +0900

    :lipstick: New icons were created.

--- vite.config.ts
+++ vite.config.ts
@@ -16,8 +16,6 @@ const manifest = defineManifest(({ mode }) => {
 		author,
 		description,
 		icons: {
-			'16': 'icons/icon16.png',
-			'32': 'icons/icon32.png',
 			'48': 'icons/icon48.png',
 			'128': 'icons/icon128.png',
 		},
